d2a4de65091682fe9dfeca083ec733686804d54a
lrebelo2 <lucasrebelo2@gmail.com>
2015-03-13 14:41:27 -0300

    Exercicios 1 e 2

---
+++ Ex1_media3.py
@@ -0,0 +1,8 @@
+def media():
+     a=float(raw_input())
+     b=float(raw_input())
+     c=float(raw_input())
+     m=(a+b+c)/3.0;
+     print m
+	 
+media()
---
+++ Ex2_Par.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+
+def is_even():
+    x=int(raw_input())%2
+    if x==0:
+        print "O numero digitado é par!"
+    else:
+        print "O numero digitado não é par!"
+is_even()
