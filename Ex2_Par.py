f031f7f2e24016ce8a01a4b7e5576b4f0402560b
lrebelo2 <lucasrebelo2@gmail.com>
2015-03-20 10:12:29 -0300

    mudanca raw_input

--- Ex2_Par.py
+++ Ex2_Par.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 
 def is_even():
-    x=int(raw_input())%2
+    x=int(raw_input("Digite o numero para verificar se é par:"))%2
     if x==0:
         print "O numero digitado é par!"
     else:
