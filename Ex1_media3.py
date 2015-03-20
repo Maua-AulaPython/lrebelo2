e399514a10a8c74ced0612e2a3348310f5e21982
lrebelo2 <lucasrebelo2@gmail.com>
2015-03-20 10:10:35 -0300

    Update Ex1_media3.py

--- Ex1_media3.py
+++ Ex1_media3.py
@@ -1,7 +1,7 @@
 def media():
-     a=float(raw_input())
-     b=float(raw_input())
-     c=float(raw_input())
+     a=float(raw_input("Digite nota 1:"))
+     b=float(raw_input("Digite nota 2:"))
+     c=float(raw_input("Digite nota 3:"))
      m=(a+b+c)/3.0;
      print m
 	 
